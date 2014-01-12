386bb3b8b9a960d91e3405ff4249014f011eaff7
supercclank <supercclank@gmail.com>
2014-01-12 02:53:12 -0500

    Intent added to start login...
    app opens to login screen now

--- FinancialPlanner/src/com/example/financialplanner/MainActivity.java
+++ FinancialPlanner/src/com/example/financialplanner/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.financialplanner;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Intent logInScreenIntent = new Intent(this, LoginActivity.class);
+		startActivity(logInScreenIntent);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
