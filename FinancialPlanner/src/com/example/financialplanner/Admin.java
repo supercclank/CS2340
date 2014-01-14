498440d5f7943ac789e8e928dbd729fbe6ec2c74
supercclank <supercclank@gmail.com>
2014-01-13 22:03:37 -0500

    Creation of Admin Class...
    class used to reset the passwords of users….

---
+++ FinancialPlanner/src/com/example/financialplanner/Admin.java
@@ -0,0 +1,26 @@
+package com.example.financialplanner;
+
+import java.util.HashMap;
+
+public class Admin{
+	private String username;
+	private String password;
+	//constructor for Admin
+	public Admin(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+	//checks to see if passwords match (for log in, need to rewrite, plain text = bad)
+	public boolean checkPass(String password){
+		if(this.password.equals(password)){
+			return true;
+		}
+		return false;
+	}
+	//returns a new user using the old userName with a new password, to over-write user in storage hashmap
+	public User resetPassword(User user, String password){
+		User newUser = new User(user.getUserName(),password);
+		return newUser;
+	}
+	
+}
