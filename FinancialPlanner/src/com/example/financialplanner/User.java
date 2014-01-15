1364d12716cc54e4c06c6116536dd4ac5100509c
Edward Yu <edwardyu236@gmail.com>
2014-01-15 14:54:26 -0500

    minor edit to a function

--- FinancialPlanner/src/com/example/financialplanner/User.java
+++ FinancialPlanner/src/com/example/financialplanner/User.java
@@ -4,35 +4,32 @@ import java.util.ArrayList;
 //Class that defines a user, keeps track of their accounts, username and password
 ///TODO - implement encryption of some type for password Storing (plain text = bad)
 public class User {
-	private ArrayList<Account> accounts;
-	private String userName;
-	private String password;
-	public User(String userName, String password){
-		accounts = new ArrayList<Account>();
-		this.userName = userName;
-		this.password = password;
-	}
-	//returns all accounts that belong to this user
-	public ArrayList<Account> getAccounts(){
-		return accounts;
-	}
-	//checks to see if passwords match (for log in, need to rewrite, plain text = bad)
-	public boolean checkPass(String password){
-		if(this.password.equals(password)){
-			return true;
-		}
-		return false;
-	}
-	//adds account to this users history
-	public boolean addAccount(Account account){
-		return accounts.add(account);
-	}
-	//deletes account from this users history
-	public boolean deleteAccount(Account account){
-		return (accounts.remove(account));
-	}
-	//
-	public String getUserName(){
-		return userName;
-	}
+    private ArrayList<Account> accounts;
+    private String userName;
+    private String password;
+    public User(String userName, String password){
+        accounts = new ArrayList<Account>();
+        this.userName = userName;
+        this.password = password;
+    }
+    //returns all accounts that belong to this user
+    public ArrayList<Account> getAccounts(){
+        return accounts;
+    }
+    //checks to see if passwords match (for log in, need to rewrite, plain text = bad)
+    public boolean checkPass(String password){
+        return this.password.equals(password));
+    }
+    //adds account to this users history
+    public boolean addAccount(Account account){
+        return accounts.add(account);
+    }
+    //deletes account from this users history
+    public boolean deleteAccount(Account account){
+        return (accounts.remove(account));
+    }
+    //
+    public String getUserName(){
+        return userName;
+    }
 }
