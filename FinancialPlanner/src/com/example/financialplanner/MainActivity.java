3bdb9034b307ae8eed7f63ae2eb8065e407df78d
nolasaint <nolasaint@github.fake>
2014-01-16 22:49:07 -0500

    More code formatting

--- FinancialPlanner/src/com/example/financialplanner/MainActivity.java
+++ FinancialPlanner/src/com/example/financialplanner/MainActivity.java
@@ -10,9 +10,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Intent logInScreenIntent = new Intent(this, LoginActivity.class);
+
 		startActivity(logInScreenIntent);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 	}
 
 	@Override
@@ -20,6 +22,7 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+
 	}
 
 }
