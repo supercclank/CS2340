d932d8debd4bd98c9996dfa4c804d9845d1981c7
Edward Yu <edwardyu236@gmail.com>
2014-02-26 21:58:22 -0500

    auto-gen files

--- FinancialPlanner/gen/com/example/financialplanner/R.java
+++ FinancialPlanner/gen/com/example/financialplanner/R.java
@@ -24,18 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f08000e;
-        public static final int action_settings=0x7f08000d;
+        public static final int action_forgot_password=0x7f08000f;
+        public static final int action_settings=0x7f08000e;
         public static final int button1=0x7f080007;
         public static final int button2=0x7f080009;
         public static final int login=0x7f080005;
         public static final int login_form=0x7f080002;
         public static final int login_status=0x7f080000;
         public static final int login_status_message=0x7f080001;
-        public static final int newuserpassword=0x7f08000b;
-        public static final int newuseruser=0x7f08000a;
+        public static final int newuserpassword=0x7f08000c;
+        public static final int newuseruser=0x7f08000b;
         public static final int password=0x7f080004;
-        public static final int register_button=0x7f08000c;
+        public static final int register_button=0x7f08000d;
+        public static final int register_form=0x7f08000a;
         public static final int sign_in_button=0x7f080006;
         public static final int textView1=0x7f080008;
         public static final int username=0x7f080003;
