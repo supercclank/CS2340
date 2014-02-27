d2cd8862e3a608ac5f5ddc3a3cd83dbd390b8f5f
supercclank <supercclank@gmail.com>
2014-02-27 13:57:28 -0500

    fixed all the things...
    valid data checks updated, creation of a 2nd alert box for when account
    creation fails, looks ugly cause it is ugly (really ugly), going to go
    back later today and clear rogue comments, document code and made it
    readable…

--- FinancialPlanner/src/com/example/financialplanner/AccountActivity.java
+++ FinancialPlanner/src/com/example/financialplanner/AccountActivity.java
@@ -10,9 +10,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -21,14 +24,18 @@ public class AccountActivity extends Activity {
 	private String json;
 	private Register register;
 	private Gson gson;
+	private String jsonString;
 	private AlertDialog.Builder createAccount;
 	private EditText accountDisplay;
 	private EditText accountName;
+	private EditText interestRate;
+	private EditText startingBalance;
+	private AlertDialog.Builder accountTaken;
 	private View v;
 	private List<Button> buttons;
 	private LinearLayout accountPane;
 	private LinearLayout layout;
-	private String jsonString;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +48,27 @@ public class AccountActivity extends Activity {
 		register = gson.fromJson(json, Register.class);
 		createAccount = new AlertDialog.Builder(this);
 		createAccount.setTitle("Account Creation Form");
+		accountTaken = new AlertDialog.Builder(this);
+		accountTaken.setTitle("Conflict with existing account, please resubmit the form");
+		
 		accountDisplay = new EditText(this);
 		accountName = new EditText(this);
+		interestRate = new EditText(this);
+		interestRate.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+		startingBalance = new EditText(this);
+		startingBalance.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
 		accountDisplay.setHint("Display Name");
 		accountName.setHint("Detailed Account Name");
+		startingBalance.setHint("Starting Balance");
+		interestRate.setHint("Interest Rate");
+		
 		layout = new LinearLayout(this);
 	    layout.setOrientation(LinearLayout.VERTICAL);
 	    layout.addView(accountDisplay);
 	    layout.addView(accountName);
+	    layout.addView(startingBalance);
+	    layout.addView(interestRate);
+	    layout.setVerticalScrollBarEnabled(true);
 	    createAccount.setView(layout);
 	    System.out.println(register.getUser().getAccounts());
 	    for (Account a: register.getUser().getAccounts()) {
@@ -56,21 +76,77 @@ public class AccountActivity extends Activity {
             b.setText(a.getDisplayName());
             b.setTag(a.getName());
             accountPane.addView(b);
+            LayoutParams params =  b.getLayoutParams();
+            params.width = LayoutParams.MATCH_PARENT;
+            b.setLayoutParams(params);
+            b.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					 Button b = (Button) v;
+					 finish();
+					 Intent intent = new Intent(AccountActivity.this, DetailActivity.class);
+					 jsonString = gson.toJson(register);
+					 System.out.println(jsonString);
+					 intent.putExtra("register", jsonString);
+					 //b.getTag().toString(),,0,0)
+					 intent.putExtra("account", gson.toJson(register.getUser().getAccounts().get(register.getUser().getAccounts().indexOf(new Account(b.getText().toString(), b.getTag().toString(), 0, 0)))));
+					 System.out.println("back button pressed");
+					 //register.resetUser();
+					 startActivity(intent);
+					
+				}
+            	
+            });
+            buttons.add(b);
 	    }
 	    createAccount.setNegativeButton("OK", new DialogInterface.OnClickListener() { 
 	        @Override
 	        public void onClick(DialogInterface dialog, int which) {
 	            String displayName = accountDisplay.getText().toString();
 	            String accountNameString = accountName.getText().toString();
-	            Account account = new Account(accountNameString, displayName, 0, 0);
-	            //System.out.println(register.getUser());
-	            //boolean bb = register.checkInformation("admin", "pass123");
-	            //System.out.println(bb);
-	            register.getUser().addAccount(account);
-	            Button b = new Button(AccountActivity.this);
-	            b.setText(displayName);
-	            b.setTag(accountName);
-	            accountPane.addView(b);
+	            String balanceString = startingBalance.getText().toString();
+	            String interestString = interestRate.getText().toString();
+	           // Account account = ;
+	            if(!(displayName.equals(""))&&!(accountNameString.equals(""))&&!(balanceString.equals(""))&&!(interestRate.equals(""))&&register.getUser().addAccount(new Account(accountNameString, displayName, Double.parseDouble(balanceString), Double.parseDouble(interestString)))){
+		            Button b = new Button(AccountActivity.this);
+		            b.setText(displayName);
+		            b.setTag(accountName);
+		            accountPane.addView(b);
+		            LayoutParams params = b.getLayoutParams();
+		            System.out.println("params is "+params);
+		            params.width = LayoutParams.MATCH_PARENT;
+		            b.setLayoutParams(params);
+		            accountDisplay.setText("");
+		            accountName.setText("");
+		            startingBalance.setText("");
+		            interestRate.setText("");
+		            buttons.add(b);
+		            b.setOnClickListener(new OnClickListener(){
+
+						@Override
+						public void onClick(View v) {
+							 Button b = (Button) v;
+							 finish();
+							 Intent intent = new Intent(AccountActivity.this, DetailActivity.class);
+							 jsonString = gson.toJson(register);
+							 System.out.println(jsonString);
+							 intent.putExtra("register", jsonString);
+							 //b.getTag().toString(),,0,0)
+							 System.out.println("the current user is: "+register.getUser());
+							 System.out.println("the account list is: "+register.getUser().getAccounts());
+							 intent.putExtra("account", gson.toJson(register.getUser().getAccounts().get(register.getUser().getAccounts().indexOf(new Account(b.getTag().toString(), b.getText().toString(), 0, 0)))));
+							 System.out.println("back button pressed");
+							 //register.resetUser();
+							 startActivity(intent);
+							
+						}
+		            	
+		            });
+	            }
+	            else {
+	            	accountTaken.show();
+	            }
 	        }
 	    });
 	    createAccount.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
@@ -79,6 +155,14 @@ public class AccountActivity extends Activity {
 	            dialog.cancel();
 	        }
 	    });
+	    accountTaken.setMessage("Conflicting information entered");
+	    accountTaken.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) { 
+				ViewGroup g = ((ViewGroup) layout.getParent());
+				g.removeView(layout);
+				createAccount.show();
+			}
+	     });
 	}
 
 	@Override
@@ -95,6 +179,7 @@ public class AccountActivity extends Activity {
 	    System.out.println(jsonString);
 	    intent.putExtra("register", jsonString);
 	    System.out.println("back button pressed");
+	    register.resetUser();
 	    startActivity(intent);
 	}
 	
@@ -110,4 +195,5 @@ public class AccountActivity extends Activity {
 		createAccount.show();
 		//user.addAccount(account);
 	}
+	
 }
