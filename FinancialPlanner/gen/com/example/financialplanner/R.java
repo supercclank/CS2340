615f38ef822ccefd919610f77225e0a1d29068a0
supercclank <supercclank@gmail.com>
2014-03-03 13:15:15 -0500

    autogenerated R...

--- FinancialPlanner/gen/com/example/financialplanner/R.java
+++ FinancialPlanner/gen/com/example/financialplanner/R.java
@@ -8,6 +8,9 @@
 package com.example.financialplanner;
 
 public final class R {
+    public static final class array {
+        public static final int transactiontype_array=0x7f070000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,24 +28,25 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int accountList=0x7f080001;
-        public static final int action_forgot_password=0x7f080011;
-        public static final int action_settings=0x7f080010;
-        public static final int button1=0x7f080000;
-        public static final int button2=0x7f08000b;
-        public static final int detailViewTextField=0x7f080002;
-        public static final int login=0x7f080008;
-        public static final int login_form=0x7f080005;
-        public static final int login_status=0x7f080003;
-        public static final int login_status_message=0x7f080004;
-        public static final int newuserpassword=0x7f08000e;
-        public static final int newuseruser=0x7f08000d;
-        public static final int password=0x7f080007;
-        public static final int register_button=0x7f08000f;
-        public static final int register_form=0x7f08000c;
-        public static final int sign_in_button=0x7f080009;
-        public static final int textView1=0x7f08000a;
-        public static final int username=0x7f080006;
+        public static final int accountList=0x7f090001;
+        public static final int action_forgot_password=0x7f090012;
+        public static final int action_settings=0x7f090011;
+        public static final int addTransaction=0x7f090003;
+        public static final int button1=0x7f090000;
+        public static final int button2=0x7f09000c;
+        public static final int detailViewTextField=0x7f090002;
+        public static final int login=0x7f090009;
+        public static final int login_form=0x7f090006;
+        public static final int login_status=0x7f090004;
+        public static final int login_status_message=0x7f090005;
+        public static final int newuserpassword=0x7f09000f;
+        public static final int newuseruser=0x7f09000e;
+        public static final int password=0x7f090008;
+        public static final int register_button=0x7f090010;
+        public static final int register_form=0x7f09000d;
+        public static final int sign_in_button=0x7f09000a;
+        public static final int textView1=0x7f09000b;
+        public static final int username=0x7f090007;
     }
     public static final class layout {
         public static final int activity_account=0x7f030000;
@@ -52,11 +56,11 @@ public final class R {
         public static final int activity_registry=0x7f030004;
     }
     public static final class menu {
-        public static final int account=0x7f070000;
-        public static final int detail=0x7f070001;
-        public static final int login=0x7f070002;
-        public static final int main=0x7f070003;
-        public static final int registry=0x7f070004;
+        public static final int account=0x7f080000;
+        public static final int detail=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int registry=0x7f080004;
     }
     public static final class string {
         public static final int action_forgot_password=0x7f05000f;
