ce9162aa4e373bc9227c6ad04cc9cc16fd578b9d
supercclank <supercclank@gmail.com>
2014-03-03 13:17:33 -0500

    detail activity update...
    added the code for an alert dialogue that allows the user to enter in
    information for backend to create transaction object

--- FinancialPlanner/src/com/example/financialplanner/DetailActivity.java
+++ FinancialPlanner/src/com/example/financialplanner/DetailActivity.java
@@ -1,11 +1,24 @@
 package com.example.financialplanner;
 
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import com.google.gson.Gson;
-
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.text.InputType;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class DetailActivity extends Activity {
@@ -14,6 +27,19 @@ public class DetailActivity extends Activity {
 	private Gson gson;
 	private String jsonString;
 	private Account account;
+	private AlertDialog.Builder addTransaction;
+	private EditText transactionName;
+	private EditText transactionValue;
+	private DatePicker addedDatePicker;
+	private DatePicker processedDatePicker;
+	private Spinner transactionType;
+	private LinearLayout transactionPane;
+	private LinearLayout layout;
+	private ScrollView scrollPane;
+	private TextView dateAddedPrompt;
+	private TextView dateProcessedPrompt;
+	private TextView transactionTypePrompt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,6 +51,65 @@ public class DetailActivity extends Activity {
 		account = gson.fromJson(json, Account.class);
 		TextView t =  (TextView) findViewById(R.id.detailViewTextField);
 		t.setText(account.toString());
+		addTransaction = new AlertDialog.Builder(this);
+		addTransaction.setTitle("Add Transaction Creation Form");
+		transactionName = new EditText(this);
+		transactionName.setHint("Enter Descriptive Transaction Name Here");
+		transactionValue = new EditText(this);
+		transactionValue.setHint("Enter the Value of the Transaction Here");
+		transactionValue.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+		dateAddedPrompt = new TextView(this);
+		dateAddedPrompt.setText("Date Added: ");
+		addedDatePicker = new DatePicker(this);
+		dateProcessedPrompt = new TextView(this);
+		dateProcessedPrompt.setText("Date Processed: ");
+		processedDatePicker = new DatePicker(this);
+		transactionType = new Spinner(this);
+		transactionTypePrompt = new TextView(this);
+		transactionTypePrompt.setText("Transaction Type");
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.transactiontype_array, android.R.layout.simple_spinner_item);
+		transactionType.setAdapter(adapter);
+		layout = new LinearLayout(this);
+	    layout.setOrientation(LinearLayout.VERTICAL);
+	    layout.addView(transactionName);
+	    layout.addView(transactionValue);
+	    layout.addView(dateAddedPrompt);
+	    layout.addView(addedDatePicker);
+	    layout.addView(dateProcessedPrompt);
+	    layout.addView(processedDatePicker);
+	    layout.addView(transactionTypePrompt);
+	    layout.addView(transactionType);
+	    layout.setVerticalScrollBarEnabled(true);
+	    scrollPane = new ScrollView(this);
+	    scrollPane.addView(layout);
+	    addTransaction.setView(scrollPane);
+	    addTransaction.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+	        @Override
+	        public void onClick(DialogInterface dialog, int which) {
+	        	Calendar c = new GregorianCalendar().getInstance();
+	        	int month = addedDatePicker.getMonth();
+	        	int date = addedDatePicker.getDayOfMonth();
+	        	int year = addedDatePicker.getYear();
+	        	c.set(year, month, date);
+	        	Date addedDate = c.getTime();
+	        	month = processedDatePicker.getMonth();
+	        	date = processedDatePicker.getDayOfMonth();
+	        	year = processedDatePicker.getYear();
+	        	c.set(year, month, date);
+	        	Date processedDate = c.getTime();
+	        	System.out.println(transactionValue.getText().toString());
+	        	double value = Double.parseDouble(transactionValue.getText().toString());
+	        	 account.performTransaction(new Transaction(Transaction.TransactionType.valueOf(transactionType.getSelectedItem().toString().toUpperCase()), value, addedDate, processedDate, transactionName.getText().toString()));
+	            TextView t =  (TextView) findViewById(R.id.detailViewTextField);
+	            t.setText(account.toString());
+	        }
+	    });
+	    addTransaction.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+	        @Override
+	        public void onClick(DialogInterface dialog, int which) {
+	            dialog.cancel();
+	        }
+	    });
 	}
 
 	@Override
@@ -34,6 +119,15 @@ public class DetailActivity extends Activity {
 		return true;
 	}
 	
+	public void addTransaction(View v){
+		ViewGroup g = ((ViewGroup) scrollPane.getParent());
+		if (g != null) {
+			g.removeView(scrollPane);
+		}
+		
+		addTransaction.setView(scrollPane);
+		addTransaction.show();
+	}
 
 	public void onBackPressed(){
 	    finish();
