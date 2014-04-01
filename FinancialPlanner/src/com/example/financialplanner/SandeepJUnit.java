21c0718c460e2b3910196c66ddd714ece031ba0f
supercclank <supercclank@gmail.com>
2014-03-31 23:03:53 -0400

    Fixed for checkstyle..

--- FinancialPlanner/src/com/example/financialplanner/SandeepJUnit.java
+++ FinancialPlanner/src/com/example/financialplanner/SandeepJUnit.java
@@ -1,42 +1,47 @@
 package com.example.financialplanner;
-
-import static org.junit.Assert.*;
+/**
+ * 
+ */
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Before;
 import org.junit.Test;
 /**
- * Tests the methods of Register
+ * Tests the methods of Register.
  * @author Sandeep
  *
  */
 public class SandeepJUnit {
 
-    /*
-     * Instance of Register to be tested
+    /**
+     * Instance of Register to be tested.
      */
     private Register tester;
     
-    @Before
+    
     /**
-     * Setup method to initialize Register object and add one valid user
+     * Setup method to initialize Register object and add one valid user.
      */
+    @Before
     public void setup() {
         tester = new Register();
-        tester.addUser("TestUser","RandomPassword!");
-        
-        
+        //CHECKSTYLE.OFF: String Literal - need of String literal used
+        tester.addUser("TestUser", "RandomPassword!");
+        //CHECKSTYLE.ON: String Literal - need of String literal used   
     }
-    @Test
+    
     /**
      * Test method for Register's checkInfromation(string,string) method.
      */
+    @Test
     public void test() {
-        assertTrue("Username: TestUser PW: RandomPassword! exists",tester.checkInformation("TestUser","RandomPassword!"));
-        assertFalse("Username: noUser PW: nothing  does not exist",tester.checkInformation("noUser","nothing"));
-        assertTrue("Admin exists",tester.checkInformation("admin_", "admin1"));
-        assertFalse("First Parameter Null",tester.checkInformation("TestUser",null));
-        assertFalse("Second Parameter Null",tester.checkInformation(null,"RandomPassword!"));
-        assertFalse("Both Parameters Null",tester.checkInformation(null,null));
+        assertTrue("Username: TestUser PW: RandomPassword! exists", tester.checkInformation("TestUser", "RandomPassword!"));
+        assertFalse("Username: noUser PW: nothing  does not exist", tester.checkInformation("noUser", "nothing"));
+        assertTrue("Admin exists", tester.checkInformation("admin_", "admin1"));
+        assertFalse("First Parameter Null", tester.checkInformation("TestUser", null));
+        assertFalse("Second Parameter Null", tester.checkInformation(null, "RandomPassword!"));
+        assertFalse("Both Parameters Null", tester.checkInformation(null, null));
         
     }
 
